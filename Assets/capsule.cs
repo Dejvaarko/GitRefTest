7540dc8b4940be1cf2527d5a466837b0bde44d85
DavidVecorek <david.vecorek@stonehengegames.com>
2016-12-29 15:13:10 +0100

    novej capsule prefab s public referenci, kterej se instancuje ve scene

---
+++ Assets/capsule.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class capsule : MonoBehaviour {
+
+    public Transform t1;
+    public Transform t2;
+    public Transform t3;
+
+}
--- Assets/test.cs
+++ Assets/test.cs
@@ -5,8 +5,14 @@ public class test : MonoBehaviour {
 
     public Transform t1;
     public Transform t2;
-    public Transform t3;
+    public GameObject t3;
     public Transform t4;
     public Transform t5;
+
+    void Start()
+    {
+        if (t3 != null)
+            Instantiate(t3);
+    }
 
 }
