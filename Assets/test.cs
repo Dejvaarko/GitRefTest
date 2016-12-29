1a8999812f65c899034cabbad361045381a48ff5
DavidVecorek <david.vecorek@stonehengegames.com>
2016-12-29 13:45:15 +0100

    1

---
+++ Assets/test.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+    public Transform t1;
+    public Transform t2;
+    public Transform t3;
+    public Transform t4;
+
+}
